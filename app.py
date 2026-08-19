512eb57d2fe97dd8edcc4c02562f3c62e3516f15
Rameshwar Barole <106742069+Rameshwar002@users.noreply.github.com>
2026-08-19 17:46:27 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1759,4 +1759,4 @@ if __name__ == "__main__":
   ║        tester/test123                 ║
   ╚═══════════════════════════════════════╝
     """)
-    app.run(host="0.0.0.0", port=8000, debug=True)
+    app.run(host="0.0.0.0", port=8000, debug=True, threaded=True)
